3f36a29800a2517bacbd6b7c416e477c7264d5a2
MUZAAQI <muzaaqi@yahoo.com>
2025-12-02 22:54:12 +0700

    refactor(db): Rename models.py to haircut_models.py and update imports
    Renamed app/model/models.py to app/model/haircut_models.py for clarity. Updated import statements in app/__init__.py and added a new controller file for haircut models.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.model import models
+from app.model import haircut_models
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
---
+++ app/controller/haircut_models_controller.py
@@ -0,0 +1,2 @@
+from app.model.haircut_models import HaircutModels
+from app import app, 
