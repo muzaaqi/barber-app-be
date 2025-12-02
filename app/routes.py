ca7d8f22de2dc35714cd5799d070954eddaf477d
MUZAAQI <muzaaqi@yahoo.com>
2025-12-02 23:35:50 +0700

    chore(route): Remove haircut models routes and add API prefix
    Deleted routes for haircut models and set a '/api' URL prefix for the Blueprint. This refactors the routing structure and removes unused controller imports.

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,8 @@
 from flask import Blueprint
-from app.controller.haircut_models_controller import get_haircut_models, get_haircut_model_by_id
 
-bp = Blueprint('api', __name__)
+bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/')
 @bp.route('/index')
 def index():
     return "Welcome to the Barber App API"
-
-@bp.route('/api/haircut-models')
-def get_haircut_models():
-    return get_haircut_models()
-
-@bp.route('/api/haircut-models/<int:model_id>')
-def get_haircut_model_by_id(model_id):
-    return get_haircut_model_by_id(model_id)
