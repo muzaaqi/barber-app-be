d755d0560061ac2b7b86cf3ed239935eae13fda4
MUZAAQI <muzaaqi@yahoo.com>
2025-12-03 22:58:31 +0700

    chore(api): Update haircut model queries and image upload
    Models are now ordered by 'choosen_count' in descending order when retrieved. Image uploads for new models specify the 'haircut-models' folder, and model creation uses 'insert()' instead of 'save()'.

--- app/controller/haircut_models.py
+++ app/controller/haircut_models.py
@@ -5,7 +5,7 @@ from app.modules.upload_r2 import upload_image
 
 def get_models():
     try:
-        haircut_models = HaircutModels.query.all()
+        haircut_models = HaircutModels.query.order_by(HaircutModels.choosen_count.desc()).all()
         data = transform(haircut_models)
         return response.ok(data, "Successfully retrieved haircut models")
     except Exception as e:
@@ -23,11 +23,11 @@ def get_model_by_id(model_id):
 
 def create_model(model_data):
     try:
-        res = upload_image(model_data.get("name"))
+        res = upload_image(model_data.get("name"), "haircut-models")
         if res == None or res[1] != 200:
             return response.bad_request("Image upload failed")
         new_model = HaircutModels(**model_data)
-        new_model.save()
+        new_model.insert()
         data = new_model.to_dict()
         return response.created(data, "Successfully created haircut model")
     except Exception as e:
