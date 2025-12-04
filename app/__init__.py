e6ce9749d6f3c06491d2f5c321ff0ae4b2e81ad6
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 07:37:58 +0700

    feat(cors): Add 127.0.0.1:5000 to CORS allowed origins
    Included http://127.0.0.1:5000 in the CORS configuration to support local development and API requests from this origin.

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    CORS(app, resources={r"/*": {"origins": ["https://bergas.muzaaqi.my.id", "http://localhost:3000"]}})
+    CORS(app, resources={r"/*": {"origins": ["https://bergas.muzaaqi.my.id", "http://localhost:3000", "http://127.0.0.1:5000"]}})
 
     db.init_app(app)
     migrate.init_app(app, db)
