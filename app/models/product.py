67a3ea4665d28860a63476752aaeac0fd19c237a
MUZAAQI <muzaaqi@yahoo.com>
2025-12-14 12:51:27 +0700

    feat: Add Product model definition
    Introduces a Product model with fields for id, name, description, price, image_url, created_at, and updated_at. This model will be used to represent products in the database.

---
+++ app/models/product.py
@@ -0,0 +1,16 @@
+from app import db
+from datetime import datetime
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
