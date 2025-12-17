c8e7a929c1eb890d964817d87fd769c0e9a97380
MUZAAQI <muzaaqi@yahoo.com>
2025-12-17 20:32:45 +0700

    refactor: Update R2 config to use new environment variable names
    Changed environment variable names for Cloudflare R2 credentials and endpoint to use the CLOUDFLARE_R2_* convention. Added support for a public flag and updated boto3 client initialization for clarity.

--- r2_config.py
+++ r2_config.py
@@ -2,15 +2,14 @@ import boto3
 from botocore.client import Config
 import os
 
-R2_ACCESS_KEY = os.getenv("R2_ACCESS_KEY")
-R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
-R2_BUCKET = os.getenv("R2_BUCKET_NAME")
-R2_ACCOUNT_ID = os.getenv("R2_ACCOUNT_ID")
-
-R2_ENDPOINT = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
+R2_ACCESS_KEY = os.getenv("CLOUDFLARE_R2_KEY_ID")
+R2_SECRET_KEY = os.getenv("CLOUDFLARE_R2_SECRET")
+R2_BUCKET = os.getenv("CLOUDFLARE_R2_BUCKET")
+R2_ENDPOINT = os.getenv("CLOUDFLARE_R2_ENDPOINT")
+R2_PUBLIC = os.getenv("CLOUDFLARE_R2_PUBLIC")
 
 s3 = boto3.client(
-    "s3",
+    service_name="s3",
     endpoint_url=R2_ENDPOINT,
     aws_access_key_id=R2_ACCESS_KEY,
     aws_secret_access_key=R2_SECRET_KEY,
