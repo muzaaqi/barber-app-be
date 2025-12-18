d8dd901e8406e47668d22a215bf0d2ab8fbcd79d
MUZAAQI <muzaaqi@yahoo.com>
2025-12-18 20:46:14 +0700

    feat: Register product transaction blueprint in routes
    Added the product_transaction_bp blueprint to the API routes to enable endpoints related to product transactions.

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app.controllers.haircut_controller import haircut_bp
 from app.controllers.user_controller import user_bp
 from app.controllers.product_controller import product_bp
 from app.controllers.haircut_transaction_controller import haircut_transaction_bp
+from app.controllers.product_transaction_controller import product_transaction_bp
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,3 +11,4 @@ api.register_blueprint(user_bp)
 api.register_blueprint(haircut_bp)
 api.register_blueprint(product_bp)
 api.register_blueprint(haircut_transaction_bp)
+api.register_blueprint(product_transaction_bp)
